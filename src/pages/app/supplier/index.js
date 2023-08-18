a8455fafcf20d8e7586299da7bbbbcddcb2491bd
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-08-18 13:12:47 +0700

    setup branch

--- src/pages/app/supplier/index.js
+++ src/pages/app/supplier/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const index = () => {
-  return <div>index</div>
+const SupplierPage = () => {
+  return <div>SupplierPage</div>
 }
 
-export default index
+export default SupplierPage
