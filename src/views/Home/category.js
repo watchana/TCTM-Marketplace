afbbaa519cdf58c11c5e56f9bab6450ddc73812b
Usagi1234 <noinon6@gmail.com>
2023-08-21 10:33:43 +0700

    1

--- src/views/Home/category.js
+++ src/views/Home/category.js
@@ -3,14 +3,14 @@ import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import Box from '@mui/material/Box';
 
-const ProductImageCard = ({ image }) => {
+const ProductImageCard = ({ name, image }) => {
   return (
     <Card>
       <CardMedia
         component="img"
         height="200"
         image={image}
-        alt="Product Image"
+        alt={name}
       />
     </Card>
   );
