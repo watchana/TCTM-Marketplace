beabcd6f0c15ecf992f523b944a98d032e08fc99
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-08-21 13:19:49 +0700

    create registerproduct page

---
+++ src/pages/app/supplier/registerproduct.js
@@ -0,0 +1,11 @@
+// *** React Import
+import React from 'react'
+
+// ** Mui imports
+import { Card } from '@mui/material'
+
+const RegisterProductPage = () => {
+  return <Card>RegisterProductPage</Card>
+}
+
+export default RegisterProductPage
