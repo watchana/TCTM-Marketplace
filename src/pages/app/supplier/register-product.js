204a41b9de7ea228edb675f3049fb301c47ee1d9
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-08-22 14:32:46 +0700

    Add upload video, show video, textfield currencies

--- src/pages/app/supplier/register-product.js
+++ src/pages/app/supplier/register-product.js
@@ -23,8 +23,36 @@ import DeleteIcon from '@mui/icons-material/Delete'
 
 const RegisterProductPage = () => {
   const [uploadImages, setUploadImages] = useState([])
+  const [uploadVideos, setUploadVideos] = useState([])
   const [openImagePreview, setOpenImagePreview] = useState(false)
   const [selectedImage, setSelectedImage] = useState(null)
+
+  const [product, setProduct] = useState({
+    name: '',
+    category: '',
+    price: '',
+    quantity: '',
+    detail: ''
+  })
+
+  const currencies = [
+    {
+      value: 'USD',
+      label: '$'
+    },
+    {
+      value: 'EUR',
+      label: '€'
+    },
+    {
+      value: 'JPY',
+      label: '¥'
+    },
+    {
+      value: 'THB',
+      label: '฿'
+    }
+  ]
 
   const handleImageChange = event => {
     const files = event.target.files
@@ -33,6 +61,20 @@ const RegisterProductPage = () => {
         .slice(0, 10)
         .map(file => URL.createObjectURL(file))
       setUploadImages([...uploadImages, ...newImages])
+    }
+  }
+
+  const handleVideoChange = event => {
+    const files = event.target.files
+    if (files && files.length > 0) {
+      const newVideos = Array.from(files)
+        .filter(file => file.type.startsWith('video/') && file.size <= 100 * 1024 * 1024) // ตรวจสอบประเภทและขนาดของวิดีโอ
+        .map(file => URL.createObjectURL(file))
+
+      setUploadVideos(newVideos) // ใช้วิดีโอใหม่แทนที่ทั้งหมด
+
+      // หรือถ้าต้องการเพิ่มวิดีโอใน array เดิม ให้ใช้บรรทัดด้านล่างแทน
+      // setUploadVideos([...uploadVideos, ...newVideos]);
     }
   }
 
@@ -46,10 +88,16 @@ const RegisterProductPage = () => {
     setOpenImagePreview(false)
   }
 
-  const handleDeleteImage = index => {
-    const updatedImages = [...uploadImages]
-    updatedImages.splice(index, 1) // ลบรูปภาพที่ต้องการออกจากอาเรย์
-    setUploadImages(updatedImages)
+  const handleDeleteMedia = (index, mediaType) => {
+    if (mediaType === 'image') {
+      const updatedImages = [...uploadImages]
+      updatedImages.splice(index, 1)
+      setUploadImages(updatedImages)
+    } else if (mediaType === 'video') {
+      const updatedVideos = [...uploadVideos]
+      updatedVideos.splice(index, 1)
+      setUploadVideos(updatedVideos)
+    }
   }
 
   useEffect(() => {
@@ -68,12 +116,11 @@ const RegisterProductPage = () => {
         <Box sx={{ my: 4 }} border={1} borderColor='rgba(0, 0, 0, 0.2)' borderRadius={1}>
           <Grid container spacing={5} sx={{ p: 4 }}>
             <Grid item xs={12} sm={2}>
-              <Box sx={{ my: 4 }}>
+              <Box sx={{ p: 4, width: '100%', height: '100%' }}>
                 <Button
                   sx={{
                     bgcolor: 'rgba(0, 0, 0, 0.01)',
-                    width: 'full',
-                    height: 150
+                    height: '100%'
                   }}
                   component='label'
                   fullWidth
@@ -101,7 +148,7 @@ const RegisterProductPage = () => {
                           />
                           <IconButton
                             aria-label='delete'
-                            onClick={() => handleDeleteImage(index)}
+                            onClick={() => handleDeleteMedia(index, 'image')}
                             sx={{
                               position: 'absolute',
                               top: 5,
@@ -128,7 +175,7 @@ const RegisterProductPage = () => {
         <Box sx={{ my: 4 }} border={1} borderColor='rgba(0, 0, 0, 0.2)' borderRadius={1}>
           <Grid container spacing={5} sx={{ p: 4 }}>
             <Grid item xs={12} sm={2}>
-              <Box sx={{ my: 4 }}>
+              <Box sx={{ my: 4, maxWidth: '100%', justifyContent: 'center', display: 'flex' }}>
                 <Button
                   sx={{
                     bgcolor: 'rgba(0, 0, 0, 0.01)',
@@ -139,31 +186,56 @@ const RegisterProductPage = () => {
                   fullWidth
                 >
                   Upload Video
-                  <input type='file' accept='image/*' hidden multiple onChange={handleImageChange} />
+                  <input type='file' accept='video/*' hidden multiple onChange={handleVideoChange} />
                 </Button>
               </Box>
             </Grid>
             <Grid item xs={12} sm={10}>
               <Box sx={{ my: 4, p: 4 }} border={1} borderColor='rgba(0, 0, 0, 0.2)' borderRadius={1}>
-                {uploadImages ? <div>test</div> : <Typography variant='body1'>No Video selected</Typography>}
+                {uploadVideos.length > 0 ? (
+                  <div>
+                    {uploadVideos.map((video, index) => (
+                      <div key={index} style={{ position: 'relative', marginBottom: '20px' }}>
+                        <video
+                          key={index}
+                          src={video}
+                          controls
+                          style={{ display: 'block', maxWidth: '100%', maxHeight: '80vh', margin: 'auto' }} // ปรับขนาดและตำแหน่งให้แสดงตรงกลาง
+                        />
+                        <IconButton
+                          aria-label='delete'
+                          onClick={() => handleDeleteMedia(index, 'video')}
+                          sx={{
+                            position: 'absolute',
+                            top: 5,
+                            right: 5,
+                            color: 'white',
+                            bgcolor: 'rgba(0, 0, 0, 0.5)'
+                          }}
+                        >
+                          <DeleteIcon />
+                        </IconButton>
+                      </div>
+                    ))}
+                  </div>
+                ) : (
+                  <Typography variant='body1'>No Video selected</Typography>
+                )}
               </Box>
-              <Box></Box>
             </Grid>
           </Grid>
         </Box>
         <Grid container spacing={5}>
           <Grid item xs={12}>
             <Typography>ชื่อสินค้า</Typography>
-            <TextField fullWidth id='product-name' label='กรอกชื่อสินค้า' variant='outlined' />
+            <TextField
+              fullWidth
+              id='product-name'
+              value={product.name}
+              onChange={e => setProduct({ ...product, name: e.target.value })}
+            />
           </Grid>
-          <Grid item xs={12}>
-            <Typography>ราคาสินค้า</Typography>
-            <TextField fullWidth id='product-price' label='กรอกชื่อสินค้า' variant='outlined' />
-          </Grid>
-          <Grid item xs={12}>
-            <Typography>จำนวนสินค้า</Typography>
-            <TextField fullWidth id='product-quantity' label='กรอกจำนวนสินค้า' variant='outlined' />
-          </Grid>
+
           <Grid item xs={12}>
             <Typography>หมวดหมู่</Typography>
             <Select fullWidth defaultValue='' id='grouped-select' label='Grouping'>
@@ -178,16 +250,30 @@ const RegisterProductPage = () => {
               <MenuItem value={4}>Option 4</MenuItem>
             </Select>
           </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Typography>Please select your currency</Typography>
+            <TextField fullWidth select id='outlined-select-currency' defaultValue='EUR'>
+              {currencies.map(option => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label} : {option.value}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+          <Grid item xs={12} sm={6} md={8}>
+            <Typography>ราคาสินค้า</Typography>
+            <TextField fullWidth id='product-price' variant='outlined' />
+          </Grid>
+
+          <Grid item xs={12}>
+            <Typography>จำนวนสินค้า</Typography>
+            <TextField fullWidth id='product-quantity' variant='outlined' />
+          </Grid>
+
           <Grid item xs={12}>
             <Typography>รายละเอียดสินค้า</Typography>
-            <TextField
-              fullWidth
-              id='product-detail'
-              multiline
-              rows={4}
-              label='กรอกรายละเอียดสินค้า'
-              variant='outlined'
-            />
+            <TextField fullWidth id='product-detail' multiline rows={4} variant='outlined' />
           </Grid>
         </Grid>
       </Card>
