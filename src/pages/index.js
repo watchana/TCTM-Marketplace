f08d5d66fda7fbe290fb0569247deb243b930481
koravin <koravinxxx@gmail.com>
2023-08-22 16:35:16 +0700

    map Card

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,146 +3,62 @@ import React from 'react'
 import { Grid, Box, TablePagination, TextField, Stack, Pagination, Button } from '@mui/material'
 import ProductCard from '../views/Home/card'
 import ProductImageCard from '../views/Home/category'
-import { useState } from 'react'
-// import productsData from '../../dummy/productsdummy';
-
-const productsPreview = [
-  {
-    name: 'พระกายแก้ว',
-    description: 'เป็นตัวประหลาดสักอย่างนี้ละ',
-    price: 100,
-    image: '../../../img/2023-08-18 090802.png' // ตั้งค่า path รูปภาพ
-  },
-  {
-    name: 'นาฬิกาหยุดเวลา',
-    description: 'ไม่รู้ไม่ชี้',
-    price: 200,
-    image: '/images/product2.jpg' // ตั้งค่า path รูปภาพ
-  },
-  {
-    name: 'พระนาราย',
-    description: 'เป็นได้แค่นั้นละ',
-    price: 100,
-    image: '../../../img/2023-08-18 090802.png' // ตั้งค่า path รูปภาพ
-  },
-  {
-    name: 'ทศกัน',
-    description: 'ยักไก่ๆ',
-    price: 200,
-    image: '/images/product2.jpg' // ตั้งค่า path รูปภาพ
-  }
-]
-const productsData = [
-  {
-    name: 'พระกายแก้ว',
-    description: 'เป็นตัวประหลาดสักอย่างนี้ละ',
-    price: 100,
-    image: '../../../img/2023-08-18 090802.png' // ตั้งค่า path รูปภาพ
-  },
-  {
-    name: 'นาฬิกาหยุดเวลา',
-    description: 'ไม่รู้ไม่ชี้',
-    price: 200,
-    image: '/images/product2.jpg' // ตั้งค่า path รูปภาพ
-  },
-  {
-    name: 'ชิ้นที่ 3',
-    description: 'ไม่รู้ไม่ชี้',
-    price: 500,
-    image: '/images/product2.jpg' // ตั้งค่า path รูปภาพ
-  },
-  {
-    name: 'ชิ้นที่ 4',
-    description: 'ไม่รู้ไม่ชี้',
-    price: 500,
-    image: '/images/product2.jpg' // ตั้งค่า path รูปภาพ
-  },
-  {
-    name: 'ชิ้นที่ 5',
-    description: 'ไม่รู้ไม่ชี้',
-    price: 500,
-    image: '/images/product2.jpg' // ตั้งค่า path รูปภาพ
-  },
-  {
-    name: 'ชิ้นที่ 6',
-    description: 'ไม่รู้ไม่ชี้',
-    price: 500,
-    image: '/images/product2.jpg' // ตั้งค่า path รูปภาพ
-  },
-  {
-    name: 'ชิ้นที่ 7',
-    description: 'ไม่รู้ไม่ชี้',
-    image: '/images/product2.jpg' // ตั้งค่า path รูปภาพ
-  },
-  {
-    name: 'ชิ้นที่ 8',
-    description: 'ไม่รู้ไม่ชี้',
-    price: 500,
-    image: '/images/product2.jpg' // ตั้งค่า path รูปภาพ
-  },
-  {
-    name: 'ชิ้นที่ 9',
-    description: 'ไม่รู้ไม่ชี้',
-    price: 500,
-    image: '/images/product2.jpg' // ตั้งค่า path รูปภาพ
-  },
-  {
-    name: 'ชิ้นที่ 10',
-    description: 'ไม่รู้ไม่ชี้',
-    price: 500,
-    image: '/images/product2.jpg' // ตั้งค่า path รูปภาพ
-  },
-  {
-    name: 'ชิ้นที่ 11',
-    description: 'ไม่รู้ไม่ชี้',
-    price: 500,
-    image: '/images/product2.jpg' // ตั้งค่า path รูปภาพ
-  },
-  {
-    name: 'ชิ้นที่ 12',
-    description: 'ไม่รู้ไม่ชี้',
-    price: 500,
-    image: '/images/product2.jpg' // ตั้งค่า path รูปภาพ
-  },
-  {
-    name: 'ชิ้นที่ 13',
-    description: 'ไม่รู้ไม่ชี้',
-    price: 500,
-    image: '/images/product2.jpg' // ตั้งค่า path รูปภาพ
-  },
-  {
-    name: 'ชิ้นที่ 14',
-    description: 'ไม่รู้ไม่ชี้',
-    price: 500,
-    image: '/images/product2.jpg' // ตั้งค่า path รูปภาพ
-  },
-  {
-    name: 'ชิ้นที่ 15',
-    description: 'ไม่รู้ไม่ชี้',
-    price: 500,
-    image: '/images/product2.jpg' // ตั้งค่า path รูปภาพ
-  },
-  {
-    name: 'ชิ้นที่ 16',
-    description: 'ไม่รู้ไม่ชี้',
-    price: 500,
-    image: '/images/product2.jpg' // ตั้งค่า path รูปภาพ
-  }
-]
+import { useState, useEffect } from 'react'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+import axios from 'axios'
 
 const Home = () => {
-  const [selectedFilter, setSelectedFilter] = useState(null);
+  // ตัวแปรเก็บค่าข้อมูล
+  const [product, setProduct] = useState('') // ตัวแปรเก็บค่า product
+  const [productname, setProductName] = useState('') // ตัวแปรเก็บค่า ชื่อ product
+  const [description, setDescription] = useState('') // ตัวแปรเก็บค่า description
+  const [price, setPrice] = useState('') // ตัวแปรเก็บค่า price
+  const [image, setImage] = useState('') // ตัวแปรเก็บค่า image
 
-  const handleFilterClick = (filter) => {
+  console.log('ข้อมูล Product', product)
+
+  // รับค่าข้อมูลจาก Api
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_API}TCTM.product.allproducts`)
+
+        const productData = response.data.message.Data
+        const names = productData.map(product => product.product_name)
+        const description = productData.map(product => product.product_description)
+        const price = productData.map(product => product.product_price)
+
+        console.log('ทดสอบ', response.data.message.Data)
+        setProduct(response.data.message.Data)
+
+        // setProductName(names)
+        // setDescription(description)
+        // setPrice(price)
+        // console.log('ข้อมูลสินค้า', response.data.message.Data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchData()
+  }, [])
+
+  // ฟังก์ชันกรองข้อมูล
+  const [selectedFilter, setSelectedFilter] = useState(null)
+
+  const handleFilterClick = filter => {
     if (selectedFilter === filter) {
       // ถ้าคลิกซ้ำกับตัวเดิม ให้ยกเลิกการเลือก
-      setSelectedFilter(null);
+      setSelectedFilter(null)
     } else {
       // ถ้าคลิกตัวใหม่ ให้เลือกตัวนั้น
-      setSelectedFilter(filter);
+      setSelectedFilter(filter)
     }
-  };
-  
+  }
+
+  // ฟังก์ชัน pagination
   const [currentPage, setCurrentPage] = useState(1)
   const [rowsPerPage, setRowsPerPage] = useState(12)
 
@@ -152,7 +68,6 @@ const Home = () => {
 
   const startIndex = (currentPage - 1) * rowsPerPage
   const endIndex = startIndex + rowsPerPage
-  const visibleProducts = productsData.slice(startIndex, endIndex)
 
   return (
     <div className='container'>
@@ -170,7 +85,7 @@ const Home = () => {
       </div>
       <Box>
         <hr />
-        </Box>
+      </Box>
       <Box>
         <Grid container spacing={6}>
           <Grid item xs={12} sm={6} md={1.5}>
@@ -222,25 +137,39 @@ const Home = () => {
       </Box>
       <h1 className='title'> ร้านค้าออนไลน์ </h1>
       <Stack direction='column' spacing={2}>
-        <div style={{ marginLeft: 'auto' }}>
-          <Pagination
-            count={Math.ceil(productsData.length / rowsPerPage)} // คำนวณจำนวนหน้า
-            page={currentPage}
-            onChange={handleChangePage}
-          />
-        </div>
         <div className='card-list'>
           <Grid container spacing={6}>
-            {visibleProducts.map((product, index) => (
-              <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
-                <ProductCard
-                  name={product.name}
-                  description={product.description}
-                  price={product.price}
-                  image={product.image}
-                />
-              </Grid>
-            ))}
+            {product ? (
+              product.map((item, index) => (
+                <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                  <Card>
+                    {/* แสดงรูปภาพสินค้า */}
+                    <CardMedia component='img' height='200' image={item.image} style={{ cursor: 'pointer' }} />
+
+                    <CardContent>
+                      {/* แสดงชื่อสินค้า */}
+                      <Typography variant='h6' component='div' style={{ cursor: 'pointer' }}>
+                        {item.product_name}
+                      </Typography>
+
+                      {/* แสดงคำอธิบายสินค้า */}
+                      {/* <Typography variant='body2' color='text.secondary'>
+                        {item.product_description}
+                      </Typography> */}
+
+                      {/* แสดงราคาสินค้า */}
+                      <Typography variant='body1' color='text.primary' fontWeight='bold'>
+                        Price: {item.product_price} THB
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              ))
+            ) : (
+              <Typography variant='body2' color='text.secondary'>
+                ไม่มีข้อมูล
+              </Typography>
+            )}
           </Grid>
         </div>
       </Stack>
@@ -249,3 +178,30 @@ const Home = () => {
 }
 
 export default Home
+
+const productsPreview = [
+  {
+    name: 'พระกายแก้ว',
+    description: 'เป็นตัวประหลาดสักอย่างนี้ละ',
+    price: 100,
+    image: '../../../img/2023-08-18 090802.png' // ตั้งค่า path รูปภาพ
+  },
+  {
+    name: 'นาฬิกาหยุดเวลา',
+    description: 'ไม่รู้ไม่ชี้',
+    price: 200,
+    image: '/images/product2.jpg' // ตั้งค่า path รูปภาพ
+  },
+  {
+    name: 'พระนาราย',
+    description: 'เป็นได้แค่นั้นละ',
+    price: 100,
+    image: '../../../img/2023-08-18 090802.png' // ตั้งค่า path รูปภาพ
+  },
+  {
+    name: 'ทศกัน',
+    description: 'ยักไก่ๆ',
+    price: 200,
+    image: '/images/product2.jpg' // ตั้งค่า path รูปภาพ
+  }
+]
--- src/views/Home/card.js
+++ src/views/Home/card.js
@@ -1,46 +1,47 @@
 // components/Card.js
-import React from 'react';
-import { useRouter } from 'next/router';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { useEffect } from 'react';
+import React from 'react'
+import { useRouter } from 'next/router'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import { useEffect } from 'react'
 
-const ProductCard = ({productId, name, description, price, image }) => {
-  const router = useRouter();
+const ProductCard = ({ productId, name, description, price, image }) => {
+  const router = useRouter()
+
+  console.log('ชื่อ', productId)
 
   const handleCardClick = () => {
-    router.push(`/product_details/${productId}`);
-  };
-
-  useEffect (() => {
-    console.log('test' ,productId);
-  }, [])
+    router.push(`/product_details/${productId}`)
+  }
 
   return (
-    <Card>
-      <CardMedia
-        component="img"
-        height="200"
-        image={image}
-        alt={name}
-        onClick={handleCardClick}
-        style={{ cursor: 'pointer' }}
-      />
-      <CardContent>
-        <Typography variant="h6" component="div" onClick={handleCardClick} style={{ cursor: 'pointer' }}>
-          {name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {description}
-        </Typography>
-        <Typography variant="body1" color="text.primary" fontWeight="bold">
-          Price: {price} THB
-        </Typography>
-      </CardContent>
-    </Card>
-  );
-};
+    <Box>
+      <Card>
+        <CardMedia
+          component='img'
+          height='200'
+          image={image}
+          alt={name}
+          onClick={handleCardClick}
+          style={{ cursor: 'pointer' }}
+        />
+        <CardContent>
+          <Typography variant='h6' component='div' onClick={handleCardClick} style={{ cursor: 'pointer' }}>
+            {name}
+          </Typography>
+          <Typography variant='body2' color='text.secondary'>
+            {description}
+          </Typography>
+          <Typography variant='body1' color='text.primary' fontWeight='bold'>
+            Price: {price} THB
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
+  )
+}
 
-export default ProductCard;
+export default ProductCard
