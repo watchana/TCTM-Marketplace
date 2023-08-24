7fa3b5e4b18e43e4e65c059afccb00b86ed1c85c
koravin <koravinxxx@gmail.com>
2023-08-24 14:04:52 +0700

    map img product

--- src/pages/designs/category.js
+++ src/pages/designs/category.js
@@ -12,11 +12,20 @@ import Container from '@mui/material/Container'
 import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
 import { CardActionArea } from '@mui/material'
+import axios from 'axios'
 
 // ** MDI Icon Imports
 import ArrowLeftThin from 'mdi-material-ui/ArrowLeftThin'
 
-const Category = () => {
+const Category = productData => {
+  if (!productData || productData.length === 0) {
+    // ตรวจสอบว่า productData เป็นค่าว่าง หรือไม่เป็น Array หรือมีข้อมูลสินค้า 0 รายการ
+    return <p>No products available.</p>
+  }
+
+  const products = productData.productData
+  console.log(products)
+
   return (
     <>
       <Container maxWidth='xl'>
@@ -50,7 +59,7 @@ const Category = () => {
             <Grid container spacing={2}>
               <Grid item>
                 <Button variant='contained' sx={{ width: '120px', height: '40px' }}>
-                  Filter 1
+                  cutting machine
                 </Button>
               </Grid>
               <Grid item>
@@ -70,223 +79,67 @@ const Category = () => {
           <Box sx={{ width: '100%' }}>
             <Grid container spacing={10}>
               {/* ======================================= map ========================================= */}
-              <Grid item>
-                <Card sx={{ width: '190px', height: '280px', bgcolor: '#fff', borderRadius: '10px' }}>
-                  <CardActionArea>
-                    <Box sx={{ width: '100%', height: '70%', padding: '10px 7px 3px' }}>
-                      {/* ใส่รูป */}
-                      <CardMedia
-                        component='img'
-                        height='200px'
-                        image='https://www.freecodecamp.org/news/content/images/size/w2000/2021/08/imgTag.png'
-                        sx={{ borderRadius: '10px' }}
-                      />
-                    </Box>
-                    <Box sx={{ width: '100%', height: '30%', paddingLeft: 2.5, paddingTop: 2 }}>
-                      <Box>
-                        <Typography
-                          variant='h6'
-                          sx={{ fontWeight: 600, overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}
-                        >
-                          Product Name
-                        </Typography>
+              {products.map((product, index) => (
+                <Grid item key={product.product_id}>
+                  <Card sx={{ width: '190px', height: '280px', bgcolor: '#fff', borderRadius: '10px' }}>
+                    <CardActionArea>
+                      <Box sx={{ width: '100%', height: '70%', padding: '10px 7px 3px' }}>
+                        {/* ใส่รูป */}
+                        <CardMedia
+                          component='img'
+                          height='200px'
+                          image={`/imgTctmProduct/${product.image_file_name}`}
+                          sx={{ borderRadius: '10px' }}
+                        />
                       </Box>
-                      <Box>
-                        <Typography variant='body1' sx={{ fontWeight: 600 }}>
-                          $ 100
-                        </Typography>
+                      <Box sx={{ width: '100%', height: '30%', paddingLeft: 2.5, paddingTop: 2 }}>
+                        <Box>
+                          <Typography
+                            variant='h6'
+                            sx={{ fontWeight: 600, overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}
+                          >
+                            {product.product_name}
+                          </Typography>
+                        </Box>
+                        <Box>
+                          <Typography variant='body1' sx={{ fontWeight: 600 }}>
+                            $ {product.product_price}
+                          </Typography>
+                        </Box>
                       </Box>
-                    </Box>
-                  </CardActionArea>
-                </Card>
-              </Grid>
-              {/* ======================================= ลบได้ ========================================= */}
-              <Grid item>
-                <Card sx={{ width: '190px', height: '280px', bgcolor: '#fff', borderRadius: '10px' }}>
-                  <CardActionArea>
-                    <Box sx={{ width: '100%', height: '70%', padding: '10px 7px 3px' }}>
-                      {/* ใส่รูป */}
-                      <CardMedia
-                        component='img'
-                        height='200px'
-                        image='https://www.freecodecamp.org/news/content/images/size/w2000/2021/08/imgTag.png'
-                        sx={{ borderRadius: '10px' }}
-                      />
-                    </Box>
-                    <Box sx={{ width: '100%', height: '30%', paddingLeft: 2.5, paddingTop: 2 }}>
-                      <Box>
-                        <Typography
-                          variant='h6'
-                          sx={{ fontWeight: 600, overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}
-                        >
-                          Product Name
-                        </Typography>
-                      </Box>
-                      <Box>
-                        <Typography variant='body1' sx={{ fontWeight: 600 }}>
-                          $ 100
-                        </Typography>
-                      </Box>
-                    </Box>
-                  </CardActionArea>
-                </Card>
-              </Grid>{' '}
-              <Grid item>
-                <Card sx={{ width: '190px', height: '280px', bgcolor: '#fff', borderRadius: '10px' }}>
-                  <CardActionArea>
-                    <Box sx={{ width: '100%', height: '70%', padding: '10px 7px 3px' }}>
-                      {/* ใส่รูป */}
-                      <CardMedia
-                        component='img'
-                        height='200px'
-                        image='https://www.freecodecamp.org/news/content/images/size/w2000/2021/08/imgTag.png'
-                        sx={{ borderRadius: '10px' }}
-                      />
-                    </Box>
-                    <Box sx={{ width: '100%', height: '30%', paddingLeft: 2.5, paddingTop: 2 }}>
-                      <Box>
-                        <Typography
-                          variant='h6'
-                          sx={{ fontWeight: 600, overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}
-                        >
-                          Product Name
-                        </Typography>
-                      </Box>
-                      <Box>
-                        <Typography variant='body1' sx={{ fontWeight: 600 }}>
-                          $ 100
-                        </Typography>
-                      </Box>
-                    </Box>
-                  </CardActionArea>
-                </Card>
-              </Grid>{' '}
-              <Grid item>
-                <Card sx={{ width: '190px', height: '280px', bgcolor: '#fff', borderRadius: '10px' }}>
-                  <CardActionArea>
-                    <Box sx={{ width: '100%', height: '70%', padding: '10px 7px 3px' }}>
-                      {/* ใส่รูป */}
-                      <CardMedia
-                        component='img'
-                        height='200px'
-                        image='https://www.freecodecamp.org/news/content/images/size/w2000/2021/08/imgTag.png'
-                        sx={{ borderRadius: '10px' }}
-                      />
-                    </Box>
-                    <Box sx={{ width: '100%', height: '30%', paddingLeft: 2.5, paddingTop: 2 }}>
-                      <Box>
-                        <Typography
-                          variant='h6'
-                          sx={{ fontWeight: 600, overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}
-                        >
-                          Product Name
-                        </Typography>
-                      </Box>
-                      <Box>
-                        <Typography variant='body1' sx={{ fontWeight: 600 }}>
-                          $ 100
-                        </Typography>
-                      </Box>
-                    </Box>
-                  </CardActionArea>
-                </Card>
-              </Grid>{' '}
-              <Grid item>
-                <Card sx={{ width: '190px', height: '280px', bgcolor: '#fff', borderRadius: '10px' }}>
-                  <CardActionArea>
-                    <Box sx={{ width: '100%', height: '70%', padding: '10px 7px 3px' }}>
-                      {/* ใส่รูป */}
-                      <CardMedia
-                        component='img'
-                        height='200px'
-                        image='https://www.freecodecamp.org/news/content/images/size/w2000/2021/08/imgTag.png'
-                        sx={{ borderRadius: '10px' }}
-                      />
-                    </Box>
-                    <Box sx={{ width: '100%', height: '30%', paddingLeft: 2.5, paddingTop: 2 }}>
-                      <Box>
-                        <Typography
-                          variant='h6'
-                          sx={{ fontWeight: 600, overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}
-                        >
-                          Product Name
-                        </Typography>
-                      </Box>
-                      <Box>
-                        <Typography variant='body1' sx={{ fontWeight: 600 }}>
-                          $ 100
-                        </Typography>
-                      </Box>
-                    </Box>
-                  </CardActionArea>
-                </Card>
-              </Grid>{' '}
-              <Grid item>
-                <Card sx={{ width: '190px', height: '280px', bgcolor: '#fff', borderRadius: '10px' }}>
-                  <CardActionArea>
-                    <Box sx={{ width: '100%', height: '70%', padding: '10px 7px 3px' }}>
-                      {/* ใส่รูป */}
-                      <CardMedia
-                        component='img'
-                        height='200px'
-                        image='https://www.freecodecamp.org/news/content/images/size/w2000/2021/08/imgTag.png'
-                        sx={{ borderRadius: '10px' }}
-                      />
-                    </Box>
-                    <Box sx={{ width: '100%', height: '30%', paddingLeft: 2.5, paddingTop: 2 }}>
-                      <Box>
-                        <Typography
-                          variant='h6'
-                          sx={{ fontWeight: 600, overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}
-                        >
-                          Product Name
-                        </Typography>
-                      </Box>
-                      <Box>
-                        <Typography variant='body1' sx={{ fontWeight: 600 }}>
-                          $ 100
-                        </Typography>
-                      </Box>
-                    </Box>
-                  </CardActionArea>
-                </Card>
-              </Grid>{' '}
-              <Grid item>
-                <Card sx={{ width: '190px', height: '280px', bgcolor: '#fff', borderRadius: '10px' }}>
-                  <CardActionArea>
-                    <Box sx={{ width: '100%', height: '70%', padding: '10px 7px 3px' }}>
-                      {/* ใส่รูป */}
-                      <CardMedia
-                        component='img'
-                        height='200px'
-                        image='https://www.freecodecamp.org/news/content/images/size/w2000/2021/08/imgTag.png'
-                        sx={{ borderRadius: '10px' }}
-                      />
-                    </Box>
-                    <Box sx={{ width: '100%', height: '30%', paddingLeft: 2.5, paddingTop: 2 }}>
-                      <Box>
-                        <Typography
-                          variant='h6'
-                          sx={{ fontWeight: 600, overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}
-                        >
-                          Product Name
-                        </Typography>
-                      </Box>
-                      <Box>
-                        <Typography variant='body1' sx={{ fontWeight: 600 }}>
-                          $ 100
-                        </Typography>
-                      </Box>
-                    </Box>
-                  </CardActionArea>
-                </Card>
-              </Grid>
+                    </CardActionArea>
+                  </Card>
+                </Grid>
+              ))}
             </Grid>
           </Box>
         </Box>
       </Container>
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  try {
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_API}TCTM.product.allproducts`)
+    const productData = response.data.message.Data
+
+    // log('DDDD', response)
+
+    return {
+      props: {
+        productData: productData // ส่งข้อมูลในรูปแบบ Array
+      }
+    }
+  } catch (error) {
+    console.error(error)
+
+    return {
+      props: {
+        productData: []
+      }
+    }
+  }
 }
 
 export default Category
