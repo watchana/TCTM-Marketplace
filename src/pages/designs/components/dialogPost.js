6fd77b811f7f69a3582f92279723a6799f2d21ff
koravin <koravinxxx@gmail.com>
2023-09-04 13:35:04 +0700

    Insert My Pose and Delete My Pose

--- src/pages/designs/components/dialogPost.js
+++ src/pages/designs/components/dialogPost.js
@@ -8,8 +8,75 @@ import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
+import axios from 'axios'
 
-const DialogPost = ({ open, handleClose }) => {
+const DialogPost = ({ open, handleClose, userId }) => {
+  // นำเข้าตัวsweetalert2
+  const Swal = require('sweetalert2')
+
+  // console.log('User Dialog', userId)
+
+  // ตัวแปรเก็บค่าข้อมูล
+  const [title, setTitle] = useState('') // ข้อมูล title
+  const [description, setDescription] = useState('') // ข้อมูล description
+
+  // เซตค่า title และ description เมื่อ open มีการเปลี่ยนแปลง
+  useEffect(() => {
+    if (open) {
+      setTitle('')
+      setDescription('')
+    }
+  }, [open])
+
+  // ฟังชันเก็บค่าข้อมูล
+  const handleTitleChange = event => {
+    setTitle(event.target.value)
+  }
+
+  const handleDesChange = event => {
+    setDescription(event.target.value)
+  }
+
+  // ฟังก์ชัน Insert Data
+  const handleInsertSubmit = e => {
+    e.preventDefault()
+
+    // ตรวจสอบค่าว่างใน TextField
+    if (!title || !description) {
+      handleClose()
+      Swal.fire({
+        icon: 'error',
+        title: 'คุณกรอกข้อมูลไม่ครบ...',
+        text: 'กรุณาระบุข้อมูลให้ครบถ้วน!'
+      })
+
+      return
+    }
+
+    const data = {
+      user_id: userId,
+      req_header: title,
+      req_description: description
+    }
+
+    console.log(data)
+
+    axios
+      .post(`${process.env.NEXT_PUBLIC_API}TCTM.requirements.postrequirement`, data)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    Swal.fire({
+      icon: 'success',
+      title: 'เพิ่มข้อมูลแล้วเสร็จ กรุณารอการยืนยัน'
+    })
+
+    handleClose()
+  }
+
   return (
     <div>
       <Dialog open={open} onClose={handleClose}>
@@ -23,6 +90,8 @@ const DialogPost = ({ open, handleClose }) => {
             <TextField
               fullWidth
               label='Title'
+              value={title}
+              onChange={handleTitleChange}
               variant='outlined'
               InputProps={{
                 style: {
@@ -36,6 +105,8 @@ const DialogPost = ({ open, handleClose }) => {
               fullWidth
               multiline
               rows={5}
+              value={description}
+              onChange={handleDesChange}
               label='Description'
               variant='outlined'
               InputProps={{
@@ -45,10 +116,12 @@ const DialogPost = ({ open, handleClose }) => {
               }}
             />
           </Box>
-          <Box sx={{ width: '600px', padding: 4 }}>
+          {/* <Box sx={{ width: '600px', padding: 4 }}>
             <TextField
               fullWidth
               label='Desired Price'
+              value={price}
+              onChange={handlePriceChange}
               variant='outlined'
               InputProps={{
                 style: {
@@ -56,9 +129,9 @@ const DialogPost = ({ open, handleClose }) => {
                 }
               }}
             />
-          </Box>
+          </Box> */}
           <Box sx={{ width: '100%', padding: 4, display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
-            <Button variant='contained' color='primary' sx={{ marginRight: 4 }}>
+            <Button variant='contained' color='primary' sx={{ marginRight: 4 }} onClick={handleInsertSubmit}>
               POST
             </Button>
             <Button variant='outlined' color='secondary' onClick={handleClose}>
--- src/pages/designs/requirements.js
+++ src/pages/designs/requirements.js
@@ -1,5 +1,5 @@
 // ** React Imports
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 
 // ** MUI Imports
 import Box from '@mui/material/Box'
@@ -23,8 +23,92 @@ import ChevronRight from 'mdi-material-ui/ChevronRight'
 // ** Imports components 📨
 import DialogPost from './components/dialogPost'
 
+// ** axios import
+import axios from 'axios'
+
 const Requirements = () => {
+  // นำเข้าตัวsweetalert2
+  const Swal = require('sweetalert2')
+
   const [openDialogPost, setOpenDialogPost] = useState(false)
+
+  // ตัวแปรเก็บค่าข้อมูล
+  const [userId, setUserId] = useState('') // ข้อมูล user_Id
+  const [myPose, setMyPose] = useState('') // ข้อมูล My pose
+
+  // รับค่าข้อมูล จาก local Storage
+  useEffect(() => {
+    const userIdFromLocalStorage = localStorage.getItem('Member_Id')
+    if (userIdFromLocalStorage) {
+      setUserId(userIdFromLocalStorage)
+    }
+  }, [])
+
+  // ดึงข้อมูล My pose จาก Api
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API}TCTM.requirements.allrequirement?user_id=${userId}`
+      )
+      setMyPose(response.data.message.Data)
+    } catch (error) {
+      console.error(error)
+    }
+  }, [userId])
+
+  // เรียกใช้ fetchData ทุกครั้งที่ User Id เปลียนแปลง
+  useEffect(() => {
+    fetchData()
+  }, [userId, fetchData])
+
+  // ฟังก์ชันลบข้อมูล
+  const handleDeleteSubmit = reqId => {
+    Swal.fire({
+      title: 'คุณต้องการลบข้อมูลหรือไม่?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'ใช่',
+      cancelButtonText: 'ไม่'
+    }).then(result => {
+      if (result.isConfirmed) {
+        const data = {
+          req_id: reqId
+        }
+
+        axios
+          .put(`${process.env.NEXT_PUBLIC_API}TCTM.requirements.deleterequirement`, data)
+          .then(function (response) {
+            console.log(response)
+
+            if (response.status === 200) {
+              Swal.fire({
+                icon: 'success',
+                title: 'ลบข้อมูลแล้วเสร็จ',
+                text: 'คุณไม่สามารถกู้คืนข้อมูลได้แล้ว'
+              })
+              fetchData()
+            } else {
+              Swal.fire({
+                icon: 'error',
+                title: 'เกิดข้อผิดพลาด',
+                text: 'ไม่สามารถลบข้อมูลได้'
+              })
+            }
+          })
+          .catch(function (error) {
+            console.log(error)
+
+            Swal.fire({
+              icon: 'error',
+              title: 'เกิดข้อผิดพลาด',
+              text: 'ไม่สามารถลบข้อมูลได้'
+            })
+          })
+      } else if (result.isDenied) {
+        console.log('cancelled delete')
+      }
+    })
+  }
 
   return (
     <Container maxWidth='xl'>
@@ -55,62 +139,87 @@ const Requirements = () => {
         {/* เนื้อหา */}
         <Box sx={{ width: '100%', marginY: 4 }}>
           <Grid container spacing={10}>
-            {/* map ตรงนี้ */}
-            <Grid item xl={4} lg={4} md={4} sm={6} xs={12}>
-              <Box sx={{ width: '100%', height: '150px', bgcolor: '#fff', borderRadius: '10px' }}>
-                {/* ลิงค์ไปอีกหน้า */}
-                <ButtonBase href='/designs/requirementsDetail' sx={{ width: '100%', height: '120px' }}>
-                  <Box sx={{ width: '100%', height: '120px' }}>
-                    <Box sx={{ width: '100%', height: '50px', padding: 2 }}>
-                      {/* หัวข้อ */}
-                      <Typography
-                        variant='h4'
-                        align='start'
-                        fontSize={36}
-                        sx={{ overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}
-                      >
-                        Title
-                      </Typography>
-                    </Box>
-                    <Box sx={{ width: '100%', height: '70px', paddingY: 6, paddingX: 3 }}>
-                      {/* เนื้อหา */}
-                      <Typography
-                        variant='body2'
-                        noWrap
-                        sx={{
-                          wordWrap: 'break-word'
-                        }}
-                      >
-                        body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde
-                        suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-                        dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
-                      </Typography>
+            {myPose === undefined ? (
+              <div
+                style={{
+                  width: '100%',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  height: '300px'
+                }}
+              >
+                Loading...
+              </div>
+            ) : myPose.length === 0 ? (
+              <div
+                style={{
+                  width: '100%',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  height: '300px'
+                }}
+              >
+                No Data
+              </div>
+            ) : (
+              myPose.map(poseData => (
+                <Grid item xl={4} lg={4} md={4} sm={6} xs={12} key={poseData.req_id}>
+                  <Box sx={{ width: '100%', height: '150px', bgcolor: '#fff', borderRadius: '10px' }}>
+                    {/* ลิงค์ไปอีกหน้า */}
+                    <ButtonBase href='/designs/requirementsDetail' sx={{ width: '100%', height: '120px' }}>
+                      <Box sx={{ width: '100%', height: '120px' }}>
+                        <Box sx={{ width: '100%', height: '50px', padding: 2 }}>
+                          {/* หัวข้อ */}
+                          <Typography
+                            variant='h4'
+                            align='start'
+                            fontSize={36}
+                            sx={{ overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}
+                          >
+                            {poseData.req_header}
+                          </Typography>
+                        </Box>
+                        <Box sx={{ width: '100%', height: '70px', paddingY: 6, paddingX: 3 }}>
+                          {/* เนื้อหา */}
+                          <Typography
+                            variant='body2'
+                            noWrap
+                            sx={{
+                              wordWrap: 'break-word'
+                            }}
+                          >
+                            {poseData.req_description}
+                          </Typography>
+                        </Box>
+                      </Box>
+                    </ButtonBase>
+                    <Box
+                      sx={{
+                        width: '100%',
+                        height: '30px',
+                        paddingX: 2,
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignContent: 'flex-start'
+                      }}
+                    >
+                      <IconButton onClick={() => handleDeleteSubmit(poseData.req_id)}>
+                        {/* ปุ่มลบ */}
+                        <DeleteIcon />
+                      </IconButton>
+                      <MessageText />
                     </Box>
                   </Box>
-                </ButtonBase>
-                <Box
-                  sx={{
-                    width: '100%',
-                    height: '30px',
-                    paddingX: 2,
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignContent: 'flex-start'
-                  }}
-                >
-                  <IconButton>
-                    {/* ปุ่มลบ */}
-                    <DeleteIcon />
-                  </IconButton>
-                  <MessageText />
-                </Box>
-              </Box>
-            </Grid>
+                </Grid>
+              ))
+            )}
           </Grid>
         </Box>
       </Box>
       {/* 📨📨 Props 📨📨 */}
-      <DialogPost open={openDialogPost} handleClose={() => setOpenDialogPost(false)} />
+      <DialogPost open={openDialogPost} handleClose={() => setOpenDialogPost(false)} userId={userId} />
       {/* 📨📨 Props 📨📨 */}
     </Container>
   )
