f82d8c4654d4dfa6ad311d29ffc3c23f3fee57d5
KHAIWAN <khai_chiao@outlook.co.th>
2023-09-26 15:46:17 +0700

    add react-multi-carousel and new design homepage 80%

--- src/views/homepage/Billboard.js
+++ src/views/homepage/Billboard.js
@@ -1,27 +1,26 @@
+// ** React Imports
+import React, { useEffect, useState } from 'react'
+
+// ** Next Import
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
 // ** Material UI Imports
 import {
   Box,
-  Button,
   Card,
   CardContent,
   Container,
   CardMedia,
   Divider,
-  FormControl,
-  FormControlLabel,
   Grid,
   Hidden,
-  IconButton,
-  InputAdornment,
-  InputLabel,
-  Link,
-  OutlinedInput,
-  Switch,
-  TextField,
+  Skeleton,
   Typography
 } from '@mui/material'
+import { styled } from '@mui/material/styles'
 
-// ** Material Icons Imports
+// ** Material-UI Icons Imports
 import PaymentIcon from '@mui/icons-material/Payment'
 import MonetizationOnIcon from '@mui/icons-material/MonetizationOn'
 
@@ -29,110 +28,290 @@ import MonetizationOnIcon from '@mui/icons-material/MonetizationOn'
 import Truck from 'mdi-material-ui/Truck'
 import ClockTimeFour from 'mdi-material-ui/ClockTimeFour'
 
+// ** Axios Import
+import axios from 'axios'
+
+// React Multi Carousel
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+
+const ImagesBillboard = [
+  {
+    id: 1,
+    img: 'https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+  },
+  {
+    id: 2,
+    img: 'https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60'
+  },
+  {
+    id: 3,
+    img: 'https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+  }
+]
+
+// ** Styled components
+const BoxAdvert = styled(Box)(theme => ({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  height: '100px',
+  cursor: 'grab',
+  '&:hover': {
+    transform: 'scale(1.1)',
+    transition: 'all 0.3s ease'
+  }
+}))
+
 const Billboard = () => {
+  const [slidedata, setSlideData] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_API}TCTM.home_page.allbillboards`)
+        setSlideData(response.data.message.Data)
+        setIsLoading(false)
+      } catch (error) {
+        console.error(error)
+        setIsLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  // React Multi Carousel Responsive
+  const responsive = {
+    desktop: {
+      breakpoint: {
+        max: 3000,
+        min: 1024
+      },
+      items: 1
+    },
+    mobile: {
+      breakpoint: {
+        max: 464,
+        min: 0
+      },
+      items: 1
+    },
+    tablet: {
+      breakpoint: {
+        max: 1024,
+        min: 464
+      },
+      items: 1
+    }
+  }
+
   return (
-    <Grid container spacing={2}>
-      <Grid item xl={8} lg={8} md={8} sm={12} xs={12}>
-        <Box sx={{ width: '100%', height: { md: '350px' }, marginBottom: 4 }}>
-          <CardMedia
-            component='img'
-            image='/imgBillboard/bill1.jpeg'
-            alt='1'
-            sx={{ maxWidth: '100%', height: '100%', borderRadius: '6px', boxShadow: 5 }}
-          />
-        </Box>
-      </Grid>
-      <Hidden mdDown>
-        <Grid item xl={4} lg={4} md={4} sm={12} xs={12}>
-          <Box sx={{ width: '100%', height: '170px', marginBottom: '10px' }}>
-            <CardMedia
-              component='img'
-              image='/imgBillboard/B.png'
-              alt='2'
-              sx={{ maxWidth: '100%', height: '100%', borderRadius: '6px', boxShadow: 3 }}
-            />
-          </Box>
-          <Box sx={{ width: '100%', height: '170px' }}>
-            <CardMedia
-              component='img'
-              image='/imgBillboard/B.png'
-              alt='2'
-              sx={{ maxWidth: '100%', height: '100%', borderRadius: '6px', boxShadow: 3 }}
-            />
-          </Box>
-        </Grid>
-        <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
-          <Box
-            sx={{
-              backgroundColor: '#fff',
-              width: '100%',
-              height: '100px',
-              borderRadius: '6px',
-              marginY: '30px',
-              boxShadow: 5
-            }}
-          >
-            <Grid container direction='row' justifyContent='space-around' alignItems='center' sx={{ height: '100%' }}>
-              <Grid item>
-                <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-                  <Truck sx={{ fontSize: '50px', color: '#000', marginRight: 3 }} />
-                  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                    <Typography variant='h6' sx={{ color: '#000' }}>
-                      Fast Delivery
-                    </Typography>
-                    <Typography variant='body1' sx={{ color: '#606060' }}>
-                      Start from $ 10
-                    </Typography>
+    <Container maxWidth='xl'>
+      {/* ---------- Billboard ---------- */}
+      <Box sx={{ width: '100%', marginBottom: '40px' }}>
+        <Grid container spacing={4}>
+          {/* ---------- Main Billboard ---------- */}
+          <Grid item xs={12} md={9}>
+            {isLoading ? ( // ตรวจสอบสถานะ isLoading เพื่อแสดงรูปโหลดหรือข้อความแสดงการโหลด
+              <Skeleton variant='rectangular' width='100%' height='350px' sx={{ borderRadius: '6px' }} />
+            ) : (
+              <Box
+                sx={{
+                  width: '100%',
+                  height: '350px',
+                  maxHeight: '350px',
+                  borderRadius: '6px'
+                }}
+              >
+                <Carousel arrows={false} autoPlaySpeed={3000} infinite showDots responsive={responsive}>
+                  {/* ##### Image ##### */}
+                  {slidedata && slidedata.length > 0 ? (
+                    slidedata.map((billboard, index) => (
+                      <CardMedia
+                        key={index}
+                        component='img'
+                        height='350px'
+                        image={`imgBillboard/${billboard.bill_name}`}
+                        alt={billboard.bill_name}
+                        sx={{
+                          width: '100%',
+                          objectFit: 'cover',
+                          objectPosition: 'center',
+                          borderRadius: '6px'
+                        }}
+                      />
+                    ))
+                  ) : (
+                    <Box
+                      sx={{
+                        width: '100%',
+                        height: '350px',
+                        maxHeight: '350px',
+                        borderRadius: '6px',
+                        backgroundColor: '#3A46A7',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                      }}
+                    >
+                      <Typography variant='h6' color='#fff'>
+                        No Image
+                      </Typography>
+                    </Box>
+                  )}
+                </Carousel>
+              </Box>
+            )}
+          </Grid>
+          {/* ---------- Sub Billboard ---------- */}
+          <Grid item xs={12} md={3}>
+            <Grid container spacing={2}>
+              {/* ---------- Sub Billboard No 1 ---------- */}
+              <Grid item xs={12} sm={6} md={12}>
+                {isLoading ? ( // ตรวจสอบสถานะ isLoading เพื่อแสดงรูปโหลดหรือข้อความแสดงการโหลด
+                  <Skeleton variant='rectangular' width='100%' height='170px' sx={{ borderRadius: '6px' }} />
+                ) : (
+                  <Box
+                    sx={{
+                      width: '100%',
+                      height: '170px',
+                      maxHeight: '170px',
+                      borderRadius: '6px',
+                      backgroundColor: '#3A46A7'
+                    }}
+                  >
+                    <Carousel arrows={false} autoPlaySpeed={5000} infinite responsive={responsive}>
+                      {/* ##### Image ##### */}
+                      {slidedata && slidedata.length > 0 ? (
+                        <>{/* ##### Map Image height: '170px' ##### */}</>
+                      ) : (
+                        <Box
+                          sx={{
+                            width: '100%',
+                            height: '170px',
+                            maxHeight: '170px',
+                            borderRadius: '6px',
+                            backgroundColor: '#3A46A7',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                          }}
+                        >
+                          <Typography variant='h6' color='#fff'>
+                            No Image
+                          </Typography>
+                        </Box>
+                      )}
+                    </Carousel>
                   </Box>
-                </Box>
+                )}
               </Grid>
-              <Divider orientation='vertical' flexItem sx={{ border: 1 }} />
-              <Grid item>
-                <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-                  <MonetizationOnIcon sx={{ fontSize: '50px', color: '#000000', marginRight: 3 }} />
-                  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                    <Typography variant='h6' sx={{ color: '#000000' }}>
-                      Money Guarantee
-                    </Typography>
-                    <Typography variant='body1' sx={{ color: '#606060' }}>
-                      7 Days Back
-                    </Typography>
+              {/* ---------- Sub Billboard No 2 ---------- */}
+              <Grid item xs={12} sm={6} md={12}>
+                {isLoading ? ( // ตรวจสอบสถานะ isLoading เพื่อแสดงรูปโหลดหรือข้อความแสดงการโหลด
+                  <Skeleton variant='rectangular' width='100%' height='170px' sx={{ borderRadius: '6px' }} />
+                ) : (
+                  <Box
+                    sx={{
+                      width: '100%',
+                      height: '170px',
+                      maxHeight: '170px',
+                      borderRadius: '6px',
+                      backgroundColor: '#3A46A7'
+                    }}
+                  >
+                    <Carousel arrows={false} autoPlaySpeed={5000} infinite responsive={responsive}>
+                      {/* ##### Image ##### */}
+                      {slidedata && slidedata.length > 0 ? (
+                        <>{/* ##### Map Image height: '170px' ##### */}</>
+                      ) : (
+                        <Box
+                          sx={{
+                            width: '100%',
+                            height: '170px',
+                            maxHeight: '170px',
+                            borderRadius: '6px',
+                            backgroundColor: '#3A46A7',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                          }}
+                        >
+                          <Typography variant='h6' color='#fff'>
+                            No Image
+                          </Typography>
+                        </Box>
+                      )}
+                    </Carousel>
                   </Box>
-                </Box>
-              </Grid>
-              <Divider orientation='vertical' flexItem sx={{ border: 1 }} />
-              <Grid item>
-                <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-                  <ClockTimeFour sx={{ fontSize: '50px', color: '#000000', marginRight: 3 }} />
-                  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                    <Typography variant='h6' sx={{ color: '#000000' }}>
-                      365 Days
-                    </Typography>
-                    <Typography variant='body1' sx={{ color: '#606060' }}>
-                      For free return
-                    </Typography>
-                  </Box>
-                </Box>
-              </Grid>
-              <Divider orientation='vertical' flexItem sx={{ border: 1 }} />
-              <Grid item>
-                <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-                  <PaymentIcon sx={{ fontSize: '50px', color: '#000000', marginRight: 3 }} />
-                  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                    <Typography variant='h6' sx={{ color: '#000000' }}>
-                      Payment
-                    </Typography>
-                    <Typography variant='body1' sx={{ color: '#606060' }}>
-                      Secure system
-                    </Typography>
-                  </Box>
-                </Box>
+                )}
               </Grid>
             </Grid>
-          </Box>
+          </Grid>
         </Grid>
+      </Box>
+      {/* ---------- Advert ---------- */}
+      <Hidden mdDown>
+        <Box sx={{ width: '100%', height: '100px' }}>
+          <Card variant='outlined'>
+            <CardContent>
+              <Grid container direction='row' justifyContent='space-around'>
+                <Grid item>
+                  <BoxAdvert>
+                    <Truck sx={{ fontSize: '50px', color: '#000', marginRight: 3 }} />
+                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                      <Typography variant='h6' color='#000'>
+                        Fast Delivery
+                      </Typography>
+                      <Typography variant='body1'>Start from $ 10</Typography>
+                    </Box>
+                  </BoxAdvert>
+                </Grid>
+                <Divider orientation='vertical' flexItem sx={{ border: 1 }} />
+                <Grid item>
+                  <BoxAdvert>
+                    <MonetizationOnIcon sx={{ fontSize: '50px', color: '#000', marginRight: 3 }} />
+                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                      <Typography variant='h6' color='#000'>
+                        Money Guarantee
+                      </Typography>
+                      <Typography variant='body1'>7 Days Back</Typography>
+                    </Box>
+                  </BoxAdvert>
+                </Grid>
+                <Divider orientation='vertical' flexItem sx={{ border: 1 }} />
+                <Grid item>
+                  <BoxAdvert>
+                    <ClockTimeFour sx={{ fontSize: '50px', color: '#000', marginRight: 3 }} />
+                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                      <Typography variant='h6' color='#000'>
+                        365 Days
+                      </Typography>
+                      <Typography variant='body1'>For free return</Typography>
+                    </Box>
+                  </BoxAdvert>
+                </Grid>
+                <Divider orientation='vertical' flexItem sx={{ border: 1 }} />
+                <Grid item>
+                  <BoxAdvert>
+                    <PaymentIcon sx={{ fontSize: '50px', color: '#000', marginRight: 3 }} />
+                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                      <Typography variant='h6' color='#000'>
+                        Payment
+                      </Typography>
+                      <Typography variant='body1'>Secure system</Typography>
+                    </Box>
+                  </BoxAdvert>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Box>
       </Hidden>
-    </Grid>
+    </Container>
   )
 }
 
---
+++ src/views/homepage/NameMarket.js
@@ -0,0 +1,156 @@
+// ** React Imports
+import React, { useEffect, useState } from 'react'
+
+// ** Next Import
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+// ** Material UI Imports
+import { Box, ButtonBase, Card, Container, CardMedia, Typography } from '@mui/material'
+import { styled } from '@mui/material/styles'
+
+// ** Axios Import
+import axios from 'axios'
+
+// React Multi Carousel
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+
+const images = [
+  'https://imagen.research.google/main_gallery_images/cactus.jpg',
+  'https://imagen.research.google/main_gallery_images/an-alien-octopus-floats.jpg',
+  'https://imagen.research.google/main_gallery_images/android-mascot-made-from-bamboo.jpg',
+  'https://imagen.research.google/main_gallery_images/a-robot-couple-fine-dining.jpg',
+  'https://imagen.research.google/main_gallery_images/teddy-bear-swimming-butterfly.jpg',
+  'https://imagen.research.google/main_gallery_images/a-brain-riding-a-rocketship.jpg',
+  'https://imagen.research.google/main_gallery_images/a-dog-looking-curiously.jpg',
+  'https://imagen.research.google/main_gallery_images/the-toronto-skyline-with-google-brain-logo.jpg',
+  'https://gweb-research-imagen.web.app/compositional/A%20photo%20of%20a%20fuzzy%20panda%20wearing%20a%20sunglasses%20and%20black%20leather%20jacket%20skateboarding%20on%20a%20beach./0_.jpeg'
+]
+
+// ** Styles Imports
+const ImageButton = styled(ButtonBase)(({ theme }) => ({
+  position: 'relative',
+  height: 200,
+  [theme.breakpoints.down('sm')]: {
+    width: '100% !important', // Overrides inline-style
+    height: 100
+  },
+  '&:hover, &.Mui-focusVisible': {
+    zIndex: 1,
+    '& .MuiImageBackdrop-root': {
+      opacity: 0.15
+    },
+    '& .MuiImageMarked-root': {
+      opacity: 0
+    },
+    '& .MuiTypography-root': {
+      border: '4px solid currentColor'
+    }
+  }
+}))
+
+const Images = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: theme.palette.common.white
+}))
+
+const ImageBackdrop = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundColor: theme.palette.common.black,
+  opacity: 0.4,
+  transition: theme.transitions.create('opacity')
+}))
+
+const ImageMarked = styled('span')(({ theme }) => ({
+  height: 3,
+  width: 18,
+  backgroundColor: theme.palette.common.white,
+  position: 'absolute',
+  bottom: -2,
+  left: 'calc(50% - 9px)',
+  transition: theme.transitions.create('opacity')
+}))
+
+const NameMarket = () => {
+  // React Multi Carousel Responsive
+  const responsive = {
+    desktop: {
+      breakpoint: {
+        max: 3000,
+        min: 1024
+      },
+      items: 6
+    },
+    tablet: {
+      breakpoint: {
+        max: 1024,
+        min: 464
+      },
+      items: 3
+    },
+    mobile: {
+      breakpoint: {
+        max: 464,
+        min: 0
+      },
+      items: 1
+    }
+  }
+
+  return (
+    <Container maxWidth='xl'>
+      {/* ---------- NameMarket ---------- */}
+      <Box sx={{ width: '100%', marginTop: { sm: '0px', md: '70px' }, paddingX: '10px' }}>
+        <Carousel arrows={false} responsive={responsive}>
+          {images.map((image, index) => (
+            <Card key={image} sx={{ width: '170px', height: '200px', borderRadius: '6px' }}>
+              <ImageButton focusRipple>
+                <CardMedia
+                  component='img'
+                  image={image}
+                  alt='NameMarket'
+                  sx={{
+                    width: '170px',
+                    height: '200px',
+                    borderRadius: '6px'
+                  }}
+                />
+                <ImageBackdrop className='MuiImageBackdrop-root' />
+                <Images>
+                  <Typography
+                    component='span'
+                    variant='subtitle1'
+                    color='inherit'
+                    sx={{
+                      position: 'relative',
+                      p: 4,
+                      pt: 2,
+                      pb: theme => `calc(${theme.spacing(1)} + 6px)`
+                    }}
+                  >
+                    NameMarket
+                    <ImageMarked className='MuiImageMarked-root' />
+                  </Typography>
+                </Images>
+              </ImageButton>
+            </Card>
+          ))}
+        </Carousel>
+      </Box>
+    </Container>
+  )
+}
+
+export default NameMarket
