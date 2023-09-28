ba54bf2dca773a3df7b9a51b0c3d8029e3f0a6bf
KHAIWAN <khai_chiao@outlook.co.th>
2023-09-28 13:45:35 +0700

    add card orderReq

--- src/pages/member/order/orderReq.js
+++ src/pages/member/order/orderReq.js
@@ -1,11 +1,21 @@
+// ** React Imports
 import React, { useEffect, useState } from 'react'
-import { Container, Grid, Typography, Card, CardContent, Button, Box } from '@mui/material'
-import Total from './total'
-import Payment from './payment'
-import Tablepayment from './TablePayment'
 
-//**  Next Import
+// ** Next Import
+import Link from 'next/link'
 import { useRouter } from 'next/router'
+
+import { Container, Breadcrumbs, Grid, Hidden, Typography, Card, CardContent, Button, Box } from '@mui/material'
+
+// ** Material-UI Icons Imports
+import CreditCardIcon from '@mui/icons-material/CreditCard'
+
+// ** Material Design Icons Imports
+import ChevronRight from 'mdi-material-ui/ChevronRight'
+
+// ** Component Imports
+import Payment from './payment'
+import TablePayment from './TablePayment'
 
 //** Axios Import
 import axios from 'axios'
@@ -16,7 +26,7 @@ const OrderReq = () => {
   const { sub_id, invoice_id } = router.query
 
   // ตัวแปรเก็บค่าข้อมูล
-  const [productdata, setProductData] = useState('') // ข้อมูล ธนาคาร
+  const [productData, setProductData] = useState('') // ข้อมูล ธนาคาร
   const [megaProductdata, setMegaProductData] = useState('') // ข้อมูล สินค้า
 
   // console.log('invoice_id', invoice_id)
@@ -44,8 +54,48 @@ const OrderReq = () => {
   }, [sub_id, invoice_id])
 
   return (
-    <Container>
+    <Container maxWidth='xl'>
       <Box>
+        <Box sx={{ width: '100%' }}>
+          <Card
+            sx={{
+              height: '100px',
+              marginBottom: '30px',
+              padding: '15px 25px 20px',
+              backgroundColor: '#2d2e81',
+              border: '1px solid #primary.main'
+            }}
+          >
+            <Grid container alignItems='center'>
+              <Grid item xs={12} sm={8} md={8}>
+                <Typography variant='h4' fontSize='21px bold' color='#fff'>
+                  Payment
+                </Typography>
+                <Breadcrumbs separator={<ChevronRight />} aria-label='breadcrumb' color='#fff'>
+                  <Link href='/' passHref>
+                    <Typography color='#fff' variant='h6' fontSize='14px' sx={{ cursor: 'pointer' }}>
+                      Home
+                    </Typography>
+                  </Link>
+                  <Link href='/member/order/myoder/' passHref>
+                    <Typography color='#fff' variant='h6' fontSize='14px' sx={{ cursor: 'pointer' }}>
+                      my order
+                    </Typography>
+                  </Link>
+                  <Typography color='#fff' variant='h6' fontSize='14px'>
+                    Payment
+                  </Typography>
+                </Breadcrumbs>
+              </Grid>
+              <Hidden smDown>
+                <Grid item sm={4} md={4} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                  <CreditCardIcon sx={{ fontSize: 72, color: '#fff' }} />
+                </Grid>
+              </Hidden>
+            </Grid>
+          </Card>
+        </Box>
+
         <Grid container>
           <Grid item xs={12} sm={12} md={4}>
             <Grid container spacing={8}>
@@ -59,7 +109,7 @@ const OrderReq = () => {
 
           <Grid item xs={12} sm={12} md={8} sx={{ display: 'flex', justifyContent: 'flex-start' }}>
             <Box sx={{ width: '100%' }}>
-              <Tablepayment productdata={productdata} />
+              <TablePayment productData={productData} />
             </Box>
           </Grid>
         </Grid>
