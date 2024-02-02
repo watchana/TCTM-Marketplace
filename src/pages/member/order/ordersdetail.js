a5b53084298a2bbd428ae2b2f2b3bb5ee77f0c36
KittiponChaiwan <kittipon_om@hotmail.com>
2024-02-02 14:26:49 +0700

    fix some bug Order detail

--- src/pages/member/order/ordersdetail.js
+++ src/pages/member/order/ordersdetail.js
@@ -59,95 +59,95 @@ const Orders_Detail = () => {
 
   return (
     <Container maxWidth='xl'>
-      <Box>
-        <Box sx={{ width: '100%' }}>
-          <Card
-            sx={{
-              height: '100px',
-              marginBottom: '30px',
-              padding: '15px 25px 20px',
-              backgroundColor: '#2d2e81',
-              border: '1px solid #primary.main'
-            }}
-          >
-            <Grid container alignItems='center'>
-              <Grid item xs={12} sm={8} md={8}>
-                <Typography variant='h4' fontSize='21px bold' color='#fff'>
-                  Orders Detail
-                  {usertype === '2' ? <span> (Marker)</span> : usertype === '1' ? <span> (User)</span> : null}
-                </Typography>
-                <Breadcrumbs separator={<ChevronRight />} aria-label='breadcrumb' color='#fff'>
-                  <Link href='/' passHref>
-                    <Typography color='#fff' variant='h6' fontSize='14px'>
-                      Home
-                    </Typography>
-                  </Link>
+      <Box sx={{ width: '100%' }}>
+        <Card
+          sx={{
+            height: '100px',
+            marginBottom: '30px',
+            padding: '15px 25px 20px',
+            backgroundColor: '#2d2e81',
+            border: '1px solid #primary.main'
+          }}
+        >
+          <Grid container alignItems='center'>
+            <Grid item xs={12} sm={8} md={8}>
+              <Typography variant='h4' fontSize='21px bold' color='#fff'>
+                Orders Detail
+                {usertype === '2' ? <span> (Marker)</span> : usertype === '1' ? <span> (User)</span> : null}
+              </Typography>
+              <Breadcrumbs separator={<ChevronRight />} aria-label='breadcrumb' color='#fff'>
+                <Link href='/' passHref>
                   <Typography color='#fff' variant='h6' fontSize='14px'>
-                    Market Management
+                    Home
                   </Typography>
-                  <Typography color='#fff' variant='h6' fontSize='14px'>
-                    detail
-                  </Typography>
-                </Breadcrumbs>
-              </Grid>
-              <Hidden smDown>
-                <Grid item sm={4} md={4} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-                  <MailOutlineIcon sx={{ fontSize: 72, color: '#fff' }} />
-                </Grid>
-              </Hidden>
-            </Grid>
-          </Card>
-        </Box>
-        {/** เลขออเดอร์ */}
-        <Box sx={{ width: '100%' }}>
-          <Card
-            sx={{
-              height: '100px',
-              marginBottom: '30px',
-              padding: '15px 25px 20px',
-              border: '2px solid #primary.main'
-            }}
-          >
-            <Grid container alignItems='center'>
-              <Grid item xs={12} sm={8} md={8}>
-                <Typography variant='h5' fontSize='18px bold'>
-                  Product Id: {orderdata.product_id}
+                </Link>
+                <Typography color='#fff' variant='h6' fontSize='14px'>
+                  Market Management
                 </Typography>
-                <Typography variant='subtitle2' fontSize='14px' sx={{ p: '0px 4px 0px' }}>
-                  Below are the details order
+                <Typography color='#fff' variant='h6' fontSize='14px'>
+                  detail
                 </Typography>
-              </Grid>
-              <Hidden smDown>
-                <Grid item sm={4} md={4} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-                  <MailOutlineIcon sx={{ fontSize: 72, color: '#fff' }} />
-                </Grid>
-              </Hidden>
+              </Breadcrumbs>
             </Grid>
-          </Card>
-        </Box>
+            <Hidden smDown>
+              <Grid item sm={4} md={4} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <MailOutlineIcon sx={{ fontSize: 72, color: '#fff' }} />
+              </Grid>
+            </Hidden>
+          </Grid>
+        </Card>
+      </Box>
+      {/** เลขออเดอร์ */}
+      <Box sx={{ width: '100%' }}>
+        <Card
+          sx={{
+            height: '100px',
+            marginBottom: '30px',
+            padding: '15px 25px 20px',
+            border: '2px solid #primary.main'
+          }}
+        >
+          <Grid container alignItems='center'>
+            <Grid item xs={12} sm={8} md={8}>
+              <Typography variant='h5' fontSize='18px bold'>
+                Product Id: {orderdata.product_id}
+              </Typography>
+              <Typography variant='subtitle2' fontSize='14px' sx={{ p: '0px 4px 0px' }}>
+                Below are the details order
+              </Typography>
+            </Grid>
+            <Hidden smDown>
+              <Grid item sm={4} md={4} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <MailOutlineIcon sx={{ fontSize: 72, color: '#fff' }} />
+              </Grid>
+            </Hidden>
+          </Grid>
+        </Card>
+      </Box>
 
-        <Grid container>
-          <Grid item sm={12} md={5}>
-            <Grid container spacing={2}>
-              <Grid item sm={12} md={11} sx={{ display: 'flex', justifyContent: 'flex-start' }}>
-                <Box sx={{ width: '100%' }}>
-                  <Total productoption={productoption} orderdata={orderdata} />
-                </Box>
-              </Grid>
-              <Grid item sm={12} md={11} sx={{ display: 'flex', justifyContent: 'flex-start' }}>
-                <Box sx={{ width: '100%' }}>
-                  <Delivery orderdata={{ orderdata }} />
-                </Box>
-              </Grid>
+      <Grid container>
+        <Grid item xs={12} md={5}>
+          <Grid container spacing={2}>
+            <Grid item sm={12} md={11} sx={{ display: 'flex', justifyContent: 'flex-start' }}>
+              <Box sx={{ width: '100%' }}>
+                <Total productoption={productoption} orderdata={orderdata} />
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={12} md={11} sx={{ display: 'flex', justifyContent: 'flex-start' }}>
+              <Box sx={{ width: '100%' }}>
+                <Delivery orderdata={{ orderdata }} />
+              </Box>
             </Grid>
           </Grid>
-          <Grid container sm={12} md={7}>
-            <Grid item sm={12} sx={{ display: 'flex', justifyContent: 'flex-start' }}>
-              <Box sx={{ width: '100%' }}>
+        </Grid>
+        <Grid item xs={12} md={7}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} mb={5} sx={{ display: 'flex', justifyContent: 'flex-start' }}>
+              <Box sx={{ width: '100%', height: '500px', overflow: 'auto' }}>
                 <ShowWorkOrder />
               </Box>
             </Grid>
-            <Grid item sm={12} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'flex-start' }}>
               <Box sx={{ width: '100%' }}>
                 <Paymant
                   usertype={usertype}
@@ -160,7 +160,7 @@ const Orders_Detail = () => {
             <CheckNpost />
           </Grid>
         </Grid>
-      </Box>
+      </Grid>
     </Container>
   )
 }
--- src/pages/member/order/workorder/showworkorder.js
+++ src/pages/member/order/workorder/showworkorder.js
@@ -7,19 +7,18 @@ import { DataGrid } from '@mui/x-data-grid'
 const ShowWorkOrder = () => {
   const [data, setData] = useState([])
 
-  const router = useRouter() // use router
+  const router = useRouter()
   const { invoice_id } = router.query
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // Fetch invoice details
         const response = await axios.get(`${process.env.NEXT_PUBLIC_API}TCTM.workorder.get_work_order`, {
           params: {
             invoice_id: invoice_id
           }
         })
-        setData(response.data.message.work_order_data)
+        setData(response.data.message.work_order_data || []) // Ensure data is an array
       } catch (error) {
         console.error(error)
       }
@@ -33,9 +32,8 @@ const ShowWorkOrder = () => {
       {data && data.length > 0 ? (
         <Card
           sx={{
-            width: '100%',
             marginBottom: '30px',
-            padding: '15px 25px 20px',
+            padding: { xs: '10px', sm: '15px 25px 20px' }, // Adjust padding for different screen sizes
             border: '2px solid #primary.main',
             borderRadius: '8px'
           }}
@@ -43,23 +41,24 @@ const ShowWorkOrder = () => {
           <Typography variant='h6' sx={{ fontWeight: 'bold', marginBottom: '15px' }}>
             Work Order
           </Typography>
-          <DataGrid
-            rows={data}
-            columns={[
-              { field: 'wod_name', headerName: 'Name', width: 300 },
-              { field: 'wod_complete_quantity', headerName: 'Complete Qty', width: 150 },
-              { field: 'wod_loss_quantity', headerName: 'Process Loss Qty', width: 200 },
-              { field: 'wod_bom', headerName: 'BOM', width: 200 },
-              { field: 'wod_work_station', headerName: 'Work Station', width: 100 },
-              { field: 'wod_time', headerName: 'Time', width: 100 }
-            ]}
-            height={'300px'}
-            getRowId={row => row.wod_id} // Specify your unique identifier field here
-            sortingMode='single'
-          />
+          <Box>
+            <DataGrid
+              rows={data}
+              columns={[
+                { field: 'wod_name', headerName: 'Name', width: 300 },
+                { field: 'wod_complete_quantity', headerName: 'Complete Qty', width: 150 },
+                { field: 'wod_loss_quantity', headerName: 'Process Loss Qty', width: 200 },
+                { field: 'wod_bom', headerName: 'BOM', width: 200 },
+                { field: 'wod_work_station', headerName: 'Work Station', width: 100 },
+                { field: 'wod_time', headerName: 'Time', width: 100 }
+              ]}
+              height={{ xs: '200px', sm: '300px' }} // Adjust height for different screen sizes
+              getRowId={row => row.wod_id}
+            />
+          </Box>
         </Card>
       ) : (
-        <Typography></Typography>
+        <Typography>No data available</Typography>
       )}
     </Box>
   )
